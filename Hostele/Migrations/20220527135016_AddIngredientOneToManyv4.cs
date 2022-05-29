3db8949658d838edb5a696ab05f38230339a0c34
czikiczinv2 <kubakucza@interia.pl>
2022-05-29 16:06:10 +0200

    UserPhoto i UsurnameChangeLimit

---
+++ Hostele/Migrations/20220527135016_AddIngredientOneToManyv4.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hostele.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIngredientOneToManyv4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Recipes_Categories_CategoryId",
+                table: "Recipes");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Recipes_Categories_CategoryId",
+                table: "Recipes",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Recipes_Categories_CategoryId",
+                table: "Recipes");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Recipes_Categories_CategoryId",
+                table: "Recipes",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ Hostele/Migrations/20220529005457_photo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hostele.Migrations
+{
+    /// <inheritdoc />
+    public partial class photo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "ProfilePicture",
+                table: "AspNetUsers",
+                type: "varbinary(max)",
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UsernameChangeLimit",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfilePicture",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UsernameChangeLimit",
+                table: "AspNetUsers");
+        }
+    }
+}
