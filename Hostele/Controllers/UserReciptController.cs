8008ad239a7c5320b2dead374077bc8f032645ca
KaMeL09 <73430339+KaMeL09@users.noreply.github.com>
2022-06-07 00:41:13 +0200

    Add files via upload

---
+++ Hostele/Controllers/UserReciptController.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Hostele.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Hostele.Controllers
+{
+    public class UserReciptController : Controller
+    {
+        private readonly IWebHostEnvironment _hostEnvironment;
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+        public UserReciptController(IWebHostEnvironment hostEnvironment, IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            _hostEnvironment = hostEnvironment;
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<IActionResult> IndexAsync()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            
+            var a = await _unitOfWork.Recipe.GetAll(filter:x=>x.AppUserId== claim.Value );
+            return View(a);
+        }
+    }
+}
