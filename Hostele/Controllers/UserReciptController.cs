6be853f0c8e22c171d268958571849dfc55b3adc
Kamil Łukowicz <kl055924@student.ath.edu.pl>
2022-06-07 19:35:36 +0200

    Dodanie proste przekierowanie

--- Hostele/Controllers/UserReciptController.cs
+++ Hostele/Controllers/UserReciptController.cs
@@ -2,6 +2,11 @@
 using Hostele.Repository;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
+using System.Security.Policy;
+using Hostele.ViewModels;
+using Microsoft.CodeAnalysis.VisualBasic.Syntax;
+using Hostele.Controllers;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Hostele.Controllers
 {
@@ -18,11 +23,30 @@ namespace Hostele.Controllers
         }
         public async Task<IActionResult> IndexAsync()
         {
-            var claimsIdentity = (ClaimsIdentity)User.Identity;
-            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
-            
-            var a = await _unitOfWork.Recipe.GetAll(filter:x=>x.AppUserId== claim.Value );
+            //var claimsIdentity = (ClaimsIdentity)User.Identity;
+            //var claim = claimsIdentity.FindFirst(ClaimTypes.Email);
+            var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            //var a = await _unitOfWork.Recipe.GetAll(filter:x=>x.AppUserId== userId );
+            var a = await _unitOfWork.Recipe.GetAll2(x=>x.AppUserId== userId);
             return View(a);
+        }
+
+        
+        public async Task<IActionResult>  Details(int? id)
+        {
+            // var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            // var b = await _unitOfWork.Recipe.GetAll2(x=>x.AppUserId== userId);
+            // foreach (var temp in b)
+            // {
+            //     RedirectToAction("RecipeDetails", "Recipe");
+            //     
+            // }
+            // var a =RedirectToAction("RecipeDetails", "Recipe");
+            // return View();
+            // var a = RedirectToAction("RecipeDetails", "Recipe", id);
+            string url ="https://localhost:7078/"+ "Recipe/RecipeDetails" + "/" + id;
+            var url2 = Redirect(url);
+            return url2;
         }
     }
 }
